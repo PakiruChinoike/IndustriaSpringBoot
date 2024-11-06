8ebae3b34b10694a2bfe85ff07eccfd220d07660
projetoindustria <alexorozco@ifsul.edu.br>
2024-11-05 21:51:24 -0300

    update SecurityConfiguration.java with env variables

--- src/main/java/com/io/industry/industria/configuration/SecurityConfiguration.java
+++ src/main/java/com/io/industry/industria/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.io.industry.industria.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -16,32 +17,38 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-    
+    @Value("${sys.user}")
+    private String user;
+    @Value("${sys.passwd}")
+    private String passwd;
+    @Value("${sys.adm.user}")
+    private String admUser;
+    @Value("${sys.adm.passwd}")
+    private String admPasswd;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
             .authorizeHttpRequests(customizer -> {
-                customizer.anyRequest().permitAll();
+                customizer.requestMatchers("/public").permitAll();
+                customizer.requestMatchers("/private").hasRole("ADMIN");
+                customizer.anyRequest().authenticated();
             })
             .httpBasic(Customizer.withDefaults())
-            .formLogin(Customizer.withDefaults())
-            .csrf(customizer -> {
-                customizer.disable();
-            })
+            .formLogin(Customizer.withDefaults())    
             .build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails commonUser = User.builder()
-            .username("user")
-            .password(passwordEncoder().encode("common"))
+            .username(user)
+            .password(passwordEncoder().encode(passwd))
             .roles("USER")
             .build();
 
         UserDetails adminUser = User.builder()
-            .username("admin")
-            .password(passwordEncoder().encode("admin"))
+            .username(admUser)
+            .password(passwordEncoder().encode(admPasswd))
             .roles("USER", "ADMIN")
             .build();
 
